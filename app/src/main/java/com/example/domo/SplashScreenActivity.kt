e2a2a981488aa6e707ce1a0813bbcb3752e42f64
Nikita <nikit.mahno@yandex.ru>
2021-10-05 19:58:35 +0700

    new commit

--- app/src/main/java/com/example/domo/SplashScreenActivity.kt
+++ app/src/main/java/com/example/domo/SplashScreenActivity.kt
@@ -3,18 +3,22 @@ package com.example.domo
 import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.viewModels
 import com.example.domo.databinding.ActivitySplashScreenBinding
+import com.example.domo.viewModels.SplashScreenActivityViewModel
 
 
 class SplashScreenActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivitySplashScreenBinding
+    private val viewModel: SplashScreenActivityViewModel by viewModels()
 
     @SuppressLint("SetTextI18n")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySplashScreenBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
 
     }
 }
--- app/src/main/java/com/example/domo/viewModels/SplashScreenActivityViewModel.kt
+++ app/src/main/java/com/example/domo/viewModels/SplashScreenActivityViewModel.kt
@@ -3,7 +3,4 @@ package com.example.domo.viewModels
 import androidx.lifecycle.ViewModel
 
 class SplashScreenActivityViewModel: ViewModel() {
-
-
-
 }
